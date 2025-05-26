33e1e5fcb9b6c68bb9086c8bb032fdec4be387d0
karolsznajder <158841611+karolsznajder@users.noreply.github.com>
2025-05-26 12:46:43 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  5 18:22:27 2021
-
-@author: Przemek
-"""
-
-	
 import requests
-
-#from PIL import Image
 import time
 
-## Pobieranie pliku
-r = requests.get('https://popbabble.files.wordpress.com/2016/07/very-young-david-hasselhoff.jpg?resize=400%2C616', stream=True)
+image_url = 'https://popbabble.files.wordpress.com/2016/07/very-young-david-hasselhoff.jpg?resize=400%2C616'
+output_file = 'fotka.png'
 
-with open('fotka.png', 'wb') as f:
-    for chunk in r.iter_content(chunk_size=1024):
-        if not chunk:
-            break
-        
-        f.write(chunk)
-        
-        
-##        
+try:
+    response = requests.get(image_url, stream=True)
+    response.raise_for_status()
+
+    with open(output_file, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+except requests.exceptions.RequestException as e:
+    print(f"Błąd podczas pobierania obrazu: {e}")
+
 time.sleep(1.5)
-##im = Image.open("C:/Users/Public/BadUSB-ChangeWallpaper/fotka.png")
-##im.show()
-##time.sleep(1)
